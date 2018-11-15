ec8bba27382737fb223b87f5ea9c11326bbe636b
Carla Mendoza Dominguez <carlamendozadominguez@gmail.com>
2018-11-15 20:58:02 +0000

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <html-clock theme = "red-lcd"><html-clock>
+</template>
+
+<script>
+  import HTMLClock from './components/HTMLClock';
+export default {
+  components: {
+    'html-clock': HTMLClock
+  }
+}
+</script>
---
+++ src/components/HTMLClock.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class= "html-clock">
+    <div class = "hour">{{ hours }}</div>
+    <div class = "sep"> : </div>
+    <div class="minutes">{{ minutes }}</div>
+    <div class="sep"> : </div>
+    <div class="seconds">{{ seconds }}</div>
+</template>
+
+<style lang = "postcss" scoped>
+  .html-clock {
+    display: grid;
+    grid-template-columns: 3fr 1fr 3fr 1fr 3fr 1.5fr;
+    justify-content: center;
+    align-items: center;
+    width: 500px;
+    height: 125px;
+    font-size: 50px;
+  }
+
+  .html-clock > div {
+    display: flex;
+    justify-content: center;
+  }
+
+  .sep {
+    animation: papasitoBlink 1s linear infinite;
+  }
+  
+  @keyframes papasitoBlink {
+    0%,
+    49% { opacity: 1; }
+    
+    50%,
+    100% { opacity: 0; }
+  }
+  
+  .red-lcd { color: red; }
+  .green-lcd { color: green; }
+  .yellow-lcd { color: yellow; }
+
+</style>
+
+<script>
+export default {
+  props: ['theme'],
+  data (){
+    return {
+      hours: '--',
+      minutes: '--',
+      seconds: '--'
+    }
+  },
+   created(){
+     setInterval( () => this.updateTime(), 1000);
+  },
+
+  methods: {
+    updateSeconds() {
+      this.seconds = this.prettyTime(new Date().getSeconds());
+    },
+
+    updateMinutes() {
+      this.minutes = this.prettyTime(new Date().getMinutes());
+    },
+    
+    updateHours(){
+      this.hours = this.prettyTime(new Date().getHours());
+    },
+
+    prettyTime(time){
+      return String(time).padStart(2, "0");
+    },
+
+    updateTime(){
+      this.updateSeconds();
+      this.updateMinutes();
+      this.updateHours();
+    }
+  },
+  computed: {
+  },
+  watch: {
+    
+  },
+}
+</script>
+
+
+
+
+
